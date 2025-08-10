736c4a51225eb9aa7319d73c77da66bee7885e08
jaskaran singh <jsandha@gmail.com>
2025-08-09 20:48:36 -0400

    services using interfaces

---
+++ src/main/java/org/sandha/store/StripePaymentService.java
@@ -0,0 +1,9 @@
+package org.sandha.store;
+
+public class StripePaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount){
+        System.out.println("Stripe");
+        System.out.println("Amount " + amount);
+    }
+}
