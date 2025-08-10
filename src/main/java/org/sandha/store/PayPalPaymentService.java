5805159610ca01b251cc7cd3203c372ee3f9a2be
jaskaran singh <jsandha@gmail.com>
2025-08-09 21:55:28 -0400

    Spring with IOC and @service @Autowired used

--- src/main/java/org/sandha/store/PayPalPaymentService.java
+++ src/main/java/org/sandha/store/PayPalPaymentService.java
@@ -1,5 +1,7 @@
 package org.sandha.store;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PayPalPaymentService implements PaymentService{
     @Override
     public void processPayment(double amount){
--- src/main/java/org/sandha/store/StoreApplication.java
+++ src/main/java/org/sandha/store/StoreApplication.java
@@ -2,16 +2,20 @@ package org.sandha.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(StoreApplication.class, args);
+       ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+      var orderService =  context.getBean(OrderService.class);
 //        OrderService orderService = new OrderService(new StripePaymentService());
-        OrderService orderService = new OrderService(new PayPalPaymentService());
-        orderService.placeOrder();
-    }
+//        OrderService orderService = new OrderService(new PayPalPaymentService());
+//        OrderService orderService = new OrderService();
+//       orderService.setPaymentService(new PayPalPaymentService());
+     orderService.placeOrder();
+   }
 
 
 }
