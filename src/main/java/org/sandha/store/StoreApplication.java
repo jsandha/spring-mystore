39d9de2dde5ec4ce39773c4f471f5d0fd895a504
jaskaran singh <jsandha@gmail.com>
2025-08-09 22:43:56 -0400

    using yaml instead of application.properties file. adding values and using them

--- src/main/java/org/sandha/store/StoreApplication.java
+++ src/main/java/org/sandha/store/StoreApplication.java
@@ -9,13 +9,10 @@ public class StoreApplication {
 
     public static void main(String[] args) {
        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-      var notificationManager =  context.getBean(NotificationManager.class);
-//        OrderService orderService = new OrderService(new StripePaymentService());
-//        OrderService orderService = new OrderService(new PayPalPaymentService());
-//        OrderService orderService = new OrderService();
-//       orderService.setPaymentService(new PayPalPaymentService());
-//     orderService.placeOrder();
-        notificationManager.sendNotification("Order Placed");
+        var orderService =  context.getBean(OrderService.class);
+//        var notificationManager = context.getBean(NotificationManager.class);
+//        notificationManager.sendNotification("Order Placed");
+        orderService.placeOrder();
    }
 
 
--- src/main/java/org/sandha/store/StripePaymentService.java
+++ src/main/java/org/sandha/store/StripePaymentService.java
@@ -1,12 +1,31 @@
 package org.sandha.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("stripe")
 public class StripePaymentService implements PaymentService {
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
+
+    @Value("${stripe.enabled}")
+    private boolean enabled;
+
+    @Value("${stripe.timeout}")
+    private int timeout;
+
+    @Value("${stripe.supported-currencies}")
+    private List<String> supportedCurrencies;
+
     @Override
     public void processPayment(double amount){
         System.out.println("Stripe");
         System.out.println("Amount " + amount);
+        System.out.println("ApiUrl " + apiUrl);
+        System.out.println("Enabled " + enabled);
+        System.out.println("Timeout " + timeout);
+        System.out.println("SupportedCurrencies " + supportedCurrencies);
     }
 }
